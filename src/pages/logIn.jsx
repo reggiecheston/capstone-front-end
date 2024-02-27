452bb064be21146ec7a5ef14115b34223a058d2a
Erick Rodea <rodeaerick@yahoo.com>
2024-02-26 20:36:09 -0500

    Materialize and Navbar Are Up

---
+++ src/pages/logIn.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import "../css/login.css";
+
+
+export default function Login() {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    )
+}
