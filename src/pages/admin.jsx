4f4ad7f112c3588d6129755708475550b3b0fb72
cmatthews15 <chastitydmatthews@gmail.com>
2024-02-28 11:15:07 -0500

    task bar and footer components added

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import "../css/admin.css";
-//import TaskBar from "../components/taskBar";
+import "../css/userViews.css";
+import TaskBarAdmin from "../components/taskBarAdmin";
+import "../css/style.css";
+import Footer from "../components/footer";
 
 
 export default function Admin() {
     return (
         <div>
             <Navbar></Navbar>
+            <body>
+            <main>
+                
+            <div className="row">
+                
+            <TaskBarAdmin></TaskBarAdmin>
+            <TaskBarAdmin></TaskBarAdmin>
+            <TaskBarAdmin></TaskBarAdmin>
+            <TaskBarAdmin></TaskBarAdmin>
             
+            </div>
+
+           </main>
+           </body>
+            <Footer></Footer>
         </div>
     )
 }
--- src/pages/dev.jsx
+++ src/pages/dev.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import "../css/dev.css";
-//import TaskBar from "../components/taskBar";
+import "../css/userViews.css";
+import TaskBarDev from "../components/taskBarDev";
+import "../css/style.css";
+import Footer from "../components/footer";
 
 
 export default function Dev() {
     return (
         <div>
-            <Navbar></Navbar>
-            
+        <Navbar></Navbar>
+        <body>
+        <main>
+        <div className="row">
+        <TaskBarDev></TaskBarDev>
+        <TaskBarDev></TaskBarDev>
+        <TaskBarDev></TaskBarDev>
+        </div> 
+        </main>
+        </body>
+        <Footer></Footer>
         </div>
     )
 }
