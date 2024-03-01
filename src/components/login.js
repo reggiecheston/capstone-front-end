90250a91c46bdefd27387992df1673115e7bddc7
bevans14 <146108421+bevans14@users.noreply.github.com>
2024-03-01 15:41:19 -0500

    changes states of login form

--- src/components/login.js
+++ src/components/login.js
@@ -1,8 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/login.css";
+import Login from "./loginfunction";
+import { useNavigate } from "react-router-dom";
+
+
 
 export default function LoginForm() {
+
+const navigate = useNavigate();
+const [ email, setEmail] = useState('');
+const [ password, setPassword] = useState('');
+const handleEmailChange = (e) => {
+  setEmail(e.target.value);
+}
+const handlePasswordChange = (e) => {
+  setPassword(e.target.value)
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  const details = {
+  email: email,
+  password: password,
+  };
+
+
+    const response = await fetch('/login', {
+      
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(details)
+    
+  });
+  const role = `SELECT admin_id, developer_id FROM staff WHERE email = "${email}"`;
+  if (response.ok) {
+    console.log(response)
+    const data = await response.json();
+    console.log(data)
+    if (data.user[0].admin_id) {
+      navigate('/admin');
+    } else if (data.user[0].developer_id) {
+      navigate('/dev');
+    } else {
+      navigate('/staff');
+    }
+  }
+}
+
   return (
+
+  
+
     <>
       <link
         href="https://fonts.googleapis.com/icon?family=Material+Icons"
@@ -19,15 +69,15 @@ export default function LoginForm() {
         <center>
           <div className="container">
             <div
-              className="z-depth-1 grey lighten-4 row"
+              className=" blue-grey lighten-4 row"
               style={{
                 display: "inline-block",
                 padding: "32px 48px 0px 48px",
                 border: "1px solid #EEE",
               }}>
-              <form className="col s12">
+              <form className="col s12" method="post" onSubmit={handleSubmit}>
                 <div className="section" />
-                <h5 className="indigo-text">Please login into your account</h5>
+                <h5 className="black-text">Please login into your account</h5>
                 <div className="section" />
                 <div className="row">
                   <div className="col s12"></div>
@@ -37,6 +87,8 @@ export default function LoginForm() {
                     <input
                       className="validate"
                       type="email"
+                      value={email}
+                      onChange={(ev) => setEmail(ev.target.value)}
                       name="email"
                       id="email"
                     />
@@ -48,13 +100,15 @@ export default function LoginForm() {
                     <input
                       className="validate"
                       type="password"
+                      value={password}
+                      onChange={(ev) => setPassword(ev.target.value)}
                       name="password"
                       id="password"
                     />
                     <label htmlFor="password">Enter your password</label>
                   </div>
                   <label style={{ alignContent: "center" }}>
-                    <a className="pink-text" href="#!">
+                    <a className="deep-orange-text" href="#!">
                       <b> Forgot Password?</b>
                     </a>
                   </label>
@@ -62,18 +116,24 @@ export default function LoginForm() {
                 <br />
                 <center>
                   <div className="row">
-                    <button
+                    <button onClick={Login}
+                      id="submit"
+                      value="submit"
                       type="submit"
                       name="btn_login"
-                      className="col s12 btn btn-large waves-effect indigo">
+                      className="col s12 btn btn-large waves-effect teal lighten-2">
+                    
                       Login
                     </button>
                   </div>
+                  <a className="deep-orange-text" href="#!">
+                    {" "}
+                    Contact Support
+                  </a>
                 </center>
               </form>
             </div>
           </div>
-          <a href="#!"> Contact Support</a>
         </center>
         <div className="section" />
         <div className="section" />
---
+++ src/components/loginfunction.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+
+// if posting login info to server i need to fetch it
+// if i am fetching it i need to run a query to validate the login info
+// if i am running a query to validate the login info i need to post it to the server
+
+export default function Login() {
+  const Login = (e) => {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const submit = document.getElementById('submit');
+    console.log(email, password)
+    e.preventDefault();
+    
+    fetch('http://localhost:4000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ 
+        email: email,
+        password: password
+       })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(email, password)
+      if (email || password === undefined) {
+        console.log(data.message); // Fixed: data.message should be outside quotes to print actual message
+        return;
+      } else {
+        localStorage.clear();
+        localStorage.setItem('user', JSON.stringify(data.user));
+        window.location.href = data.user.isAdmin ? '/admin' : '/dashboard';
+      }
+    });
+  };
+}
