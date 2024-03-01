33376fc98892ade322005eb1581cb0b39f44b684
cmatthews15 <chastitydmatthews@gmail.com>
2024-03-01 16:54:53 -0500

    group push

--- src/components/taskBarAdmin.js
+++ src/components/taskBarAdmin.js
@@ -1,32 +1,17 @@
+
 import "../css/userViews.css";
 import React, { useEffect } from 'react';
 import CollapsibleComponent from './collapse';
 
 var Title = "Title"
 
-const props = {
-  Head: "Head",
-  Count: "Count",
-  Category: "Category",
-  Priority: "Priority",
-  Title: "Title",
-  Summary: "Description",
-  Date: "Date",
-  Developer: "Developer",
-  Staff: "Staff"
- 
-}
-
-
-
-
- 
 export default function TaskBarAdmin() {
-   
-  return (
+  const props = { // Define your props here
+    Head: "Head",
+    Count: "Count",
+  };
 
-   
-   
+  return (
     <>
       <link
         href="https://fonts.googleapis.com/icon?family=Material+Icons"
@@ -38,34 +23,15 @@ export default function TaskBarAdmin() {
         href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css"
       />
 
-      
-      
-   
-      
-    <div className="col s12 m3">
-      <div className="card blue-grey darken-1">
-        <div className="card-content white-text">
-          <span className="card-title">{props.Head} <div className="right">{props.Count}</div></span>
-         
-        <CollapsibleComponent></CollapsibleComponent>
-
-          
-
-
-        
-        
-
-
-
-    
-
-        
+      <div className="col s12 m3">
+        <div className="card blue-grey lighten-4">
+          <div className="card-content white-text">
+            <span className="card-title">{props.Head} <div className="right">{props.Count}</div></span>
+            <CollapsibleComponent /> 
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-     
-  
-      
     </>
   );
 }
+
